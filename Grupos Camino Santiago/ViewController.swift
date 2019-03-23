b160b757f83070fcf9bf5545e6243a254f5989f6
mperezf <miguelperezfernandez31@gmail.com>
2019-03-23 22:49:12 +0100

    Initial Commit

---
+++ Grupos Camino Santiago/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grupos Camino Santiago
+//
+//  Created by Miguel Perez on 23/03/2019.
+//  Copyright © 2019 Miguel Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
